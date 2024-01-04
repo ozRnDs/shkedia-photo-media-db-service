ceff3318d89cca06e6c8bd4a39c0f2c37def54cc
oz <oz.rnds@gmail.com>
2024-01-04 15:34:01 +0000

    test(test_authentication): Fix attribute names to fit the updated AuthService

--- tests/test_authentication/test_auth_service.py
+++ tests/test_authentication/test_auth_service.py
@@ -6,7 +6,7 @@ from fastapi import HTTPException
 
 @pytest.fixture(scope="module")
 def auth_service_fixture(jwt_key_location_fixture, mock_db_service):
-    auth_service = AuthService(db_service=mock_db_service,
+    auth_service = AuthService(user_db_service=mock_db_service,
                                 jwt_key_location=jwt_key_location_fixture)
     yield auth_service
 
